d650f5a4dd43adfd8d7934e8eefb1e0f7c68c6b5
H-JungJaeHeon <jjhking222@naver.com>
2022-05-27 11:15:07 +0900

    ButtonAnim
    ButtonAnim

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using DG.Tweening;
 
 public class TitleManager : MonoBehaviour
 {
     #region 시작 화면 UI 모음
     [Header("시작 화면 UI 모음")]
-    [SerializeField] private GameObject TitleName;
+    [SerializeField] private GameObject TitleNameObj;
+    [SerializeField] private GameObject GameSettingPopUp;
+    [Header("타이틀 버튼 오브젝트")]
     [SerializeField] private Button GameStartButton;
-    [SerializeField] private Button GameExitButton, GameSettingButton;
+    [SerializeField] private Button GameExitButton, GameSettingButton, GameSettingExitButton;
+    [SerializeField] private bool IsSettingUp;
+    private Vector3 pos;
     #endregion
 
 
@@ -20,6 +24,7 @@ public class TitleManager : MonoBehaviour
         GameStartButton.onClick.AddListener(GameStart);
         GameSettingButton.onClick.AddListener(GameSetting);
         GameExitButton.onClick.AddListener(GameExit);
+        GameSettingExitButton.onClick.AddListener(GameSettingClose);
     }
 
     // Update is called once per frame
@@ -27,10 +32,12 @@ public class TitleManager : MonoBehaviour
     {
         MoveTitleName();
     }
+ 
     private void MoveTitleName()
     {
-       TitleName.transform.position = new Vector3(0,3 + Mathf.Sin(Time.time * 0.5f) * 0.5f, 0);
+        TitleNameObj.transform.position = new Vector3(0,3 + Mathf.Sin(Time.time * 0.5f) * 0.5f, 0);
     }
+    #region 타이틀 버튼 관련 함수
     private void GameStart()
     {
         SceneManager.LoadScene(1);
@@ -43,6 +50,22 @@ public class TitleManager : MonoBehaviour
     }
     private void GameSetting()
     {
-        Debug.Log("세팅을 왜함? ㄹㅇ ㅋㅋ");
+        if(IsSettingUp == false)
+        {
+            Debug.Log("세팅을 왜함? ㄹㅇ ㅋㅋ");
+            GameSettingPopUp.transform.DOScale(1, 0.6f).SetEase(Ease.InOutQuad);
+            IsSettingUp = true;
+        }
     }
+    private void GameSettingClose()
+    {
+        if(IsSettingUp == true)
+        {
+            Debug.Log("게임 세팅 화면 종료");
+            GameSettingPopUp.transform.DOScale(0, 0.6f).SetEase(Ease.InOutQuad);
+            IsSettingUp = false;
+        }
+    }
+    #endregion
 }
+ 
