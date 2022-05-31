fcae051d23f5eddca92aea1de5d41b143c28240e
H-JungJaeHeon <jjhking222@naver.com>
2022-05-31 11:32:00 +0900

    TitleSettingPanel
    TitleSettingPanel

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -41,19 +41,16 @@ public class TitleManager : MonoBehaviour
     private void GameStart()
     {
         SceneManager.LoadScene(1);
-        Debug.Log("빠르게시작~");
     }
-    private void GameExit()
-    {
-        Debug.Log("종료할거면 왜킴? ㄹㅇ ㅋㅋ");
-        Application.Quit();
-    }
+
+    private void GameExit() => Application.Quit();
+
     private void GameSetting()
     {
         if(IsSettingUp == false)
         {
             Debug.Log("세팅을 왜함? ㄹㅇ ㅋㅋ");
-            GameSettingPopUp.transform.DOMove(new Vector3(1200,540,0), 1.2f).SetEase(Ease.InOutBack);
+            GameSettingPopUp.transform.DOMove(new Vector3(1300,540,0), 1.2f).SetEase(Ease.InOutBack);
             IsSettingUp = true;
         }
     }
@@ -62,7 +59,7 @@ public class TitleManager : MonoBehaviour
         if(IsSettingUp == true)
         {
             Debug.Log("게임 세팅 화면 종료");
-            GameSettingPopUp.transform.DOMove(new Vector3(2820, 540, 0), 1.2f).SetEase(Ease.InOutBack);
+            GameSettingPopUp.transform.DOMove(new Vector3(2700, 540, 0), 1.2f).SetEase(Ease.InOutBack);
             IsSettingUp = false;
         }
     }
