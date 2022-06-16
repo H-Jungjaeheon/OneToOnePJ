54f50739b02ce74d8aaa6185676400f9e163db0c
H-Jungjaeheon <jjhking222@naver.com>
2022-06-17 03:11:50 +0900

    GameStartAnim50%Make
    GameStartAnim50%Make

--- Assets/Script/Stage1/Stage1Manager.cs
+++ Assets/Script/Stage1/Stage1Manager.cs
@@ -7,23 +7,34 @@ using DG.Tweening;
 
 public class Stage1Manager : MonoBehaviour
 {
-    [SerializeField] private GameObject GameManager, GamePauseObj, GameStartPanelObj, GameHelpObj;
+    [SerializeField] private GameObject GameManager, GamePauseObj, GameStartPanelObj, GameHelpObj, StartPanelObj, StartPanelPos;
     [SerializeField] private Button StagePauseButton, StageContinueButton, StageExitButton, StageRestartButton, GameHelpButton;
-    [SerializeField] private bool StageStarting;
+    [SerializeField] private Image StartFaidBackGround, StartPanelImage;
+    [SerializeField] private Text[] StartText;
+    //[SerializeField] private bool StageStarting;
     public int ResultCount;
     
     private void Awake()
     {
-        GamePauseObj.SetActive(false);
-        GameManager = GameObject.Find("GameManager");
-        StagePauseButton.onClick.AddListener(()=> ClickStagePauseButton());
-        StageContinueButton.onClick.AddListener(() => ClickStageContinueButton());
-        StageExitButton.onClick.AddListener(() => ClickStageExitButton());
-        StageRestartButton.onClick.AddListener(() => ClickStageRestartButton());
+        StartSetting();
+    }
+    private void Start()
+    {
+        StartCoroutine(StartAnim(1.5f));
     }
     private void FixedUpdate()
     {
         IsStageClear();
+    }
+    private void StartSetting()
+    {
+        GamePauseObj.SetActive(false);
+        GameStartPanelObj.SetActive(true);
+        GameManager = GameObject.Find("GameManager");
+        StagePauseButton.onClick.AddListener(() => ClickStagePauseButton());
+        StageContinueButton.onClick.AddListener(() => ClickStageContinueButton());
+        StageExitButton.onClick.AddListener(() => ClickStageExitButton());
+        StageRestartButton.onClick.AddListener(() => ClickStageRestartButton());
     }
     private void IsStageClear()
     {
@@ -33,6 +44,27 @@ public class Stage1Manager : MonoBehaviour
             GameManager.GetComponent<GameManager>().StageClearCount++;
             SceneManager.LoadScene(1);
         }
+    }
+    IEnumerator StartAnim(float FaidTime)
+    {
+        float NowFaidTime = FaidTime;
+        StartPanelObj.transform.DOMove(StartPanelPos.transform.position, 1.5f);
+        yield return new WaitForSeconds(1.8f);
+        Color color = StartFaidBackGround.color;
+        Color color2 = StartPanelImage.color;
+        while (NowFaidTime >= 0)
+        {
+            color.a = NowFaidTime;
+            color2.a = NowFaidTime;
+            StartFaidBackGround.color = color;
+            StartPanelImage.color = color2;
+            for (int a = 0; a < StartText.Length; a++){
+                StartText[a].color = color;
+            }
+            NowFaidTime -= Time.deltaTime;
+            yield return null;
+        }
+        GameStartPanelObj.SetActive(false);
     }
     #region 스테이지 버튼 모음
     private void ClickStagePauseButton() => GamePauseObj.SetActive(true);
