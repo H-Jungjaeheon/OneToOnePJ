f0243ed2e97946c10da0aab76e8b3d3b471c8d47
H-JungJaeHeon <jjhking222@naver.com>
2022-07-06 04:21:32 +0900

    Stage3Game70%Make
    Stage3Game70%Make

---
+++ Assets/Script/Stage3/Customer.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Live2D.Cubism.Rendering;
+
+public class Customer : MonoBehaviour
+{
+    [Header("움직임 관련 속도 변수")]
+    [SerializeField] private float MoveSpeed;
+    [SerializeField] private float UpDownSpeed;
+    [SerializeField] CubismRenderController rendererController;
+    private bool IsUp, IsOrder, End;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartMove();
+        Discrimination();
+    }
+    private void StartMove()
+    {
+        if(transform.position.x < -7 && !End) // -1.9 -2.12
+        {
+            if (!IsUp)
+            {
+                transform.position += new Vector3(MoveSpeed * Time.deltaTime, -(UpDownSpeed * Time.deltaTime), 0);
+                if (transform.position.y <= -2.12f)
+                    IsUp = true;
+            }
+            else
+            {
+                transform.position += new Vector3(MoveSpeed * Time.deltaTime, UpDownSpeed * Time.deltaTime, 0);
+                if (transform.position.y >= -1.9f)
+                    IsUp = false;
+            }
+        }
+        else if(transform.position.x >= -7 && !IsOrder) //손 원래 위치 5, 7, 0
+        {
+            Stage3Manager.Instance.MatchesCountObj[Stage3Manager.Instance.MaxMatchesCount - 1].transform.DOScale(1, 0.5f);
+            var HandObj = Stage3Manager.Instance.HandObj;
+            HandObj.transform.position = Vector3.Lerp(HandObj.transform.position, new Vector3(5, 3.05f, 0), 0.05f);
+            if(HandObj.transform.position.y <= 3.1)
+            {
+                Stage3Manager.Instance.CustomerArrival = true;
+                animator.SetBool("IsArrival", true);
+                IsOrder = true;
+            }
+        }
+    }
+    private void Discrimination()
+    {
+        if (Stage3Manager.Instance.IsSuccess)
+        {
+            Stage3Manager.Instance.CustomerArrival = false;
+            Stage3Manager.Instance.IsSuccess = false;
+            animator.SetBool("IsArrival", false);
+            animator.SetBool("IsGood", true);
+            StartCoroutine(GoBack());
+        }
+        else if (Stage3Manager.Instance.IsFail)
+        {
+            Stage3Manager.Instance.CustomerArrival = false;
+            Stage3Manager.Instance.IsFail = false;
+            animator.SetBool("IsArrival", false);
+            animator.SetBool("IsBad", true);
+            StartCoroutine(GoBack());
+        }
+    }
+    private IEnumerator GoBack()
+    {
+        float DestroyCount = 0;
+
+        yield return new WaitForSeconds(2.5f);
+
+        animator.SetBool("IsGood", false);
+        animator.SetBool("IsBad", false);
+
+        End = true;
+        rendererController.SortingMode = CubismSortingMode.BackToFrontOrder;
+        //rendererController.SortingLayerId = -5;
+        transform.rotation = Quaternion.Euler(0,180,0);
+
+        while (true)
+        {
+            if (!IsUp)
+            {
+                transform.position += new Vector3(-MoveSpeed * Time.deltaTime, -(UpDownSpeed * Time.deltaTime), 0);
+                if (transform.position.y <= -2.12f)
+                    IsUp = true;
+            }
+            else
+            {
+                transform.position += new Vector3(-MoveSpeed * Time.deltaTime, UpDownSpeed * Time.deltaTime, 0);
+                if (transform.position.y >= -1.9f)
+                    IsUp = false;
+            }
+
+            if(DestroyCount >= 6)
+                Destroy(gameObject);
+
+            DestroyCount += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
--- Assets/Script/Stage3/Stage3Manager.cs
+++ Assets/Script/Stage3/Stage3Manager.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Stage3Manager : Stage1Manager
 {
     public static Stage3Manager Instance { get; set; }
-    [SerializeField] private int MaxRandCount;
+    [Header("현재 준 성냥의 개수")]
+    [SerializeField] private float GiveMatchesCount;
+    [Header("등장한 손님 리스트")]
+    [SerializeField] private List<GameObject> Customers = new List<GameObject>();
+    [Header("손님 프리펩")]
+    [SerializeField] private GameObject[] CustomersObj;
+    [Header("말풍선 오브젝트")]
+    public GameObject[] MatchesCountObj; //말풍선 오브젝트
+    [Header("손님 손 오브젝트")]
+    public GameObject HandObj; //손 오브젝트
+    [Header("완료 버튼")]
+    [SerializeField] private Button GiveCompleteButton;
+    public int MaxMatchesCount, RoundCount, FailCount, SuccesCount; //줘야하는 성냥 개수, 현재 라운드, 실패 카운트, 성공 카운트
+    public bool IsFail, IsSuccess, CustomerArrival;
 
     private void FixedUpdate()
     {
@@ -44,13 +59,56 @@ public class Stage3Manager : Stage1Manager
     protected override void StartSetting()
     {
         base.StartSetting();
+        GiveCompleteButton.onClick.AddListener(() => Discrimination());
         Instance = this;
-        MaxRandCount = Random.Range(1, 6);
+        CustomerCommingEvent();
     }
 
-    private void IsStageClear()
+    private void CustomerCommingEvent()
     {
-        ProgressImage.fillAmount = ResultCount / 4;
+        if(RoundCount < 4)
+        {
+            int CustomersRandCount = Random.Range(0, 3);
+            MaxMatchesCount = Random.Range(1, 6);
+            RoundCount++;
+            Customers.Add(Instantiate(CustomersObj[CustomersRandCount], new Vector3(-12.9f, -2.12f, 0), CustomersObj[CustomersRandCount].transform.rotation));
+        }
+    }
+
+    private void IsStageClear() //게임 클리어, 오버 연출 넣기
+    {
+        ProgressImage.fillAmount = ResultCount / 2;
+    }
+
+    private void Discrimination() //정 / 오답 판별
+    {
+        if (CustomerArrival)
+        {
+            if (MaxMatchesCount == GiveMatchesCount)
+            {
+                IsSuccess = true;
+                SuccesCount++;
+            }
+            else
+            {
+                IsFail = true;
+                FailCount++;
+            }
+            GiveMatchesCount = 0;
+            StartCoroutine(NextCustomerComming());
+        }
+    }
+    private IEnumerator NextCustomerComming()
+    {
+        float a = 0;
+        while(a < 7)
+        {
+            MatchesCountObj[MaxMatchesCount - 1].transform.DOScale(0, 0.5f);
+            HandObj.transform.position = Vector3.Lerp(HandObj.transform.position, new Vector3(5, 7, 0), 0.05f);
+            a += Time.deltaTime;
+            yield return null;
+        }
+        CustomerCommingEvent();
     }
     /// <summary>
     /// 스테이지 버튼 모음
